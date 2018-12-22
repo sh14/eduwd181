6bfa556bbb663eb969acc448e29830fb3a1058a2
sh14 <sh-v@ya.ru>
2018-12-22 20:52:37 +0300

    защита формы

--- wp-content/plugins/publisher/publisher.php
+++ wp-content/plugins/publisher/publisher.php
@@ -13,9 +13,30 @@ Date: 13.12.18
 
 namespace publisher;
 
+require 'includes/option-page.php';
+
+/**
+ * Форма создания/редактирования публикации
+ *
+ * @return string
+ */
 function post_form() {
+
 	ob_start();
-	include 'templates/form-post.php';
+	$author_id = 0;
+	$user_id   = get_current_user_id();
+	if ( ! empty( $_GET['id'] ) ) {
+		$post_id   = $_GET['id'];
+		$author_id = (array) get_post( $post_id );
+		$author_id = $author_id['post_author'];
+	}
+
+	if ( current_user_can( 'edit_others_posts' ) || ( current_user_can( 'edit_posts' ) && ( $author_id == $user_id || 0 == $author_id ) ) ) {
+		include 'templates/form-post.php';
+	} else {
+		include 'templates/access-denied.php';
+	}
+
 	$out = ob_get_contents();
 	ob_clean();
 
@@ -25,13 +46,46 @@ function post_form() {
 add_shortcode( 'add_post', __NAMESPACE__ . '\post_form' );
 
 function post_save() {
-	if ( ! empty( $_POST['action'] ) ) {
+
+	// проверяем, передаются ли данные формы и производится проверка валидности этой формы
+	if ( ! empty( $_POST['action'] ) && wp_verify_nonce( $_POST['form-post'], 'post_save' ) ) {
 		$data                    = $_POST;
 		$data['_p']['post_type'] = 'post';
-		$post_id                 = wp_insert_post( $data['_p'] );
 
-		wp_set_post_categories( $post_id, $data['_t'] );
-		wp_redirect( get_permalink( 87 ).'?id='.$post_id.'&event=update' );
+		// сохранение публикации и полечение id данной публикации
+		$post_id = wp_insert_post( $data['_p'] );
+
+		// установка категории поста
+		wp_set_post_categories( $post_id, $data['_t']['category'] );
+
+		// преобразование тегов в массив
+		$tags = $data['_t']['tag'];
+		$tags = str_replace( '#', ',', $tags );
+		$tags = str_replace( ' ', ',', $tags );
+		$tags = explode( ',', $tags );
+		$tags = array_filter( $tags, function ( $tag ) {
+			return ! empty( $tag );
+		} );
+
+		// указание тегов для поста
+		wp_set_post_tags( $post_id, $tags );
+
+		// обновление мета-данных
+		if ( ! empty( $data['_m'] ) ) {
+			foreach ( $data['_m'] as $key => $value ) {
+
+				$value = esc_attr( $value );
+				if ( ! empty( $value ) ) {
+					update_post_meta( $post_id, $key, $value );
+				} /*else {
+                    // удалять мета-данные следует только в крайних случаях, если вы уверены6 что это ни на что не влияет
+					delete_post_meta( $post_id, $key );
+				}*/
+			}
+		}
+
+		wp_redirect( get_permalink( intval( get_option( __NAMESPACE__ . 'editor_page' ) ) ) . '?id=' . $post_id . '&event=update' );
+		die();
 	}
 }
 
--- wp-content/plugins/publisher/templates/form-post.php
+++ wp-content/plugins/publisher/templates/form-post.php
@@ -1,12 +1,40 @@
 <?php
+echo 'editor_page<pre>';
+print_r( esc_attr( get_option( 'editor_page' ) ) );
+echo '</pre>';
 
 if ( ! empty( $_GET['id'] ) ) {
 	$post_id = $_GET['id'];
-	$post    = get_post( $post_id );
+	$post    = (array) get_post( $post_id );
+		echo '<pre>';
+		print_r( $post['post_author'] );
+		echo '</pre>';
+	$cat  = wp_get_post_categories( $post_id );
+	$tags = get_the_tag_list( '#', ' #', '', $post_id );
 
-	$cat = wp_get_post_categories( $post->ID );
+	$tags = strip_tags( $tags );
 
+	$meta = get_post_meta( $post_id );
+	$meta = array_map(function ($val){
+		return $val[0];
+	},$meta);
+
+} else {
+	$post = array(
+		'ID'           => 0,
+		'post_author'  => 0,
+		'post_date'    => '',
+		'post_content' => '',
+		'post_title'   => '',
+		'post_excerpt' => '',
+		'post_status'  => '',
+		'post_parent'  => 0,
+	);
+	$cat  = array( 0 );
+	$tags = '';
+	$meta = '';
 }
+$post['post_type'] = 'post';
 
 $post_status = array(
 	array(
@@ -28,7 +56,7 @@ $post_status = array(
 
 $options = '';
 foreach ( $post_status as $i => $status ) {
-	if ( $status['value'] == $post->post_status ) {
+	if ( $status['value'] == $post['post_status'] ) {
 		$post_status[ $i ]['selected'] = true;
 
 	}
@@ -38,7 +66,7 @@ foreach ( $post_status as $i => $status ) {
 
 ?>
 
-	<form action="" class="form" method="post">
+<form action="" class="form" method="post">
 	<div class="form__group">
 		<div class="form__label"><?php _e( 'Заголовок', 'publisher' ); ?></div>
 		<?php wp_dropdown_categories( array(
@@ -54,7 +82,7 @@ foreach ( $post_status as $i => $status ) {
 	</div>
 	<div class="form__group">
 		<div class="form__label"><?php _e( 'Заголовок', 'publisher' ); ?></div>
-		<input type="text" class="form__control" name="_p[post_title]" value="<?php echo $post->post_title; ?>">
+		<input type="text" class="form__control" name="_p[post_title]" value="<?php echo $post['post_title']; ?>">
 	</div>
 	<div class="form__group">
 		<div class="form__label"><?php _e( 'Заголовок', 'publisher' ); ?></div>
@@ -64,16 +92,51 @@ foreach ( $post_status as $i => $status ) {
 	</div>
 	<div class="form__group">
 		<div class="form__label"><?php _e( 'Контент', 'publisher' ); ?></div>
-		<textarea class="form__control" name="_p[post_content]"><?php echo $post->post_content; ?></textarea>
-		<button type="submit" class="form__button"><?php _e( 'Сохранить', 'publisher' ); ?></button>
-		<input type="hidden" name="_p[ID]" value="<?php echo $post_id; ?>">
-		<input type="hidden" name="action" value="post_save">
+		<textarea class="form__control" name="_p[post_content]"><?php echo $post['post_content'] ?></textarea>
 	</div>
-<?php
-if ( ! empty( $post_id ) ) {
-	?>
-	<a target="_blank" href="<?php echo home_url( '?p=' . $post_id . '&preview=true' ); ?>"><?php _e( 'Предпросмотр', 'publisher' ); ?></a>
+	<div class="form__group">
+		<div class="form__label"><?php _e( 'Теги', 'publisher' ); ?></div>
+		<input type="text" class="form__control" name="_t[tag]" value="<?php echo $tags; ?>"/>
+
+	</div>
+	<div class="form__group">
+		<div class="form__label"><?php _e( 'Рейтинг', 'publisher' ); ?></div>
+		<input
+				type="number"
+				class="form__control"
+				min="0"
+				max="5"
+				step="1"
+				name="_m[rating]"
+				value="<?php echo $meta['rating']; ?>"
+		/>
+
+	</div>
+	<div class="form__group">
+		<div class="form__label"><?php _e( 'Вкусно', 'publisher' ); ?></div>
+		<input
+				type="number"
+				class="form__control"
+				min="0"
+				max="5"
+				step="1"
+				name="_m[taste]"
+				value="<?php echo $meta['taste']; ?>"
+		/>
+
+	</div>
+
+	<button type="submit" class="form__button"><?php _e( 'Сохранить', 'publisher' ); ?></button>
+	<input type="hidden" name="_p[ID]" value="<?php echo $post_id; ?>">
+	<input type="hidden" name="action" value="post_save">
+	<?php wp_nonce_field( 'post_save', 'form-post' ); ?>
+
 	<?php
+	if ( ! empty( $post_id ) ) {
+		?>
+		<a target="_blank"
+		   href="<?php echo home_url( '?p=' . $post_id . '&preview=true' ); ?>"><?php _e( 'Предпросмотр', 'publisher' ); ?></a>
+		<?php
 	}
 	?>
 	<div class="form form_info">
