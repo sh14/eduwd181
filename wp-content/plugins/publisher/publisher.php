bbaca63425d68c9ff54d9cf0f86a8889b060165b
sh14 <sh-v@ya.ru>
2018-12-27 21:05:18 +0300

    рефакторинг кода с возможностью сохранения публикации в случае отказа работы через javascript(если у кого-то js отключен в браузере)

--- wp-content/plugins/publisher/publisher.php
+++ wp-content/plugins/publisher/publisher.php
@@ -13,7 +13,25 @@ Date: 13.12.18
 
 namespace publisher;
 
+function get_version() {
+	return '1.0';
+}
+
+function get_plugin_path() {
+
+	$plugin_path = plugin_dir_path( __FILE__ );
+	$plugin_path = explode( '/', $plugin_path );
+	$plugin_name = array_filter( $plugin_path, function ( $value ) {
+		return ! empty( $value );
+	} );
+	$plugin_name = end( $plugin_name );
+
+	return trailingslashit( plugins_url() ) . $plugin_name;
+}
+
+
 require 'includes/option-page.php';
+require 'includes/ajax.php';
 
 /**
  * Форма создания/редактирования публикации
@@ -45,11 +63,17 @@ function post_form() {
 
 add_shortcode( 'add_post', __NAMESPACE__ . '\post_form' );
 
-function post_save() {
+/**
+ * Функция сохранения публикации
+ *
+ * @param $data
+ *
+ * @return array
+ */
+function post_save( $data ) {
 
 	// проверяем, передаются ли данные формы и производится проверка валидности этой формы
-	if ( ! empty( $_POST['action'] ) && wp_verify_nonce( $_POST['form-post'], 'post_save' ) ) {
-		$data                    = $_POST;
+	if ( ! empty( $data['action'] ) && wp_verify_nonce( $data['form-post'], 'post_save' ) ) {
 		$data['_p']['post_type'] = 'post';
 
 		// сохранение публикации и полечение id данной публикации
@@ -84,11 +108,48 @@ function post_save() {
 			}
 		}
 
-		wp_redirect( get_permalink( intval( get_option( __NAMESPACE__ . 'editor_page' ) ) ) . '?id=' . $post_id . '&event=update' );
-		die();
+		// если данные пришли по AJAX
+		if ( ! empty( $data['ajax'] ) ) {
+
+			// возвращаем id поста
+			return array( 'post_id' => $post_id, );
+		} else {
+
+			// осуществляем редирект
+			wp_redirect( get_permalink( intval( get_option( __NAMESPACE__ . 'editor_page' ) ) ) . '?id=' . $post_id . '&event=update' );
+			die();
+		}
+	}
+
+	return array();
+}
+
+/**
+ * Функция осуществляет сохранение данных с последующим редиректом, если в браузере пользователя не работает JavaScript
+ */
+function classic_post_save() {
+	if ( ! empty( $_POST ) && ! empty( $_POST['js_disabled'] ) ) {
+		$data         = $_POST;
+		$data['ajax'] = 0;
+		post_save( $data );
 	}
 }
 
-add_action( 'init', __NAMESPACE__ . '\post_save' );
+add_action( 'init', __NAMESPACE__ . '\classic_post_save' );
+
+
+function enqueue_styles() {
+
+	//wp_enqueue_style( 'my-plugin', trailingslashit( get_plugin_path() ) . 'style.css' );
+
+	wp_enqueue_script( __NAMESPACE__, trailingslashit( get_plugin_path() ) . 'assets/js/functions.js', array(), get_version(), true );
+
+	wp_localize_script( __NAMESPACE__, __NAMESPACE__, array(
+		'ajax_url' => admin_url( 'admin-ajax.php' ),
+	) );
+}
+
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_styles' );
+
 
 // eof
